a7917c6afd0110293a17ea78da2df0ae2f686e02
xeusteerapat <xeus085@gmail.com>
2021-10-25 18:38:06 +0700

    :sparkles: [EDIT] update auth resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,20 @@
 import { randomUUID } from "crypto";
+import { authorizeWithGithub } from "../libs/auth.js";
 
-let photos = [];
 export const resolvers = {
   Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos,
+    totalPhotos: async (_, __, { db }) => {
+      return await db.collection("photos").estimatedDocumentCount();
+    },
+    allPhotos: async (_, __, { db }) => {
+      return await db.collection("photos").find().toArray();
+    },
+    totalUsers: async (_, __, { db }) => {
+      return await db.collection("users").estimatedDocumentCount();
+    },
+    allUsers: async (_, __, { db }) => {
+      return await db.collection("users").find().toArray();
+    },
   },
   Mutation: {
     postPhoto(_, args) {
@@ -16,6 +26,47 @@ export const resolvers = {
       photos.push(newPhoto);
 
       return newPhoto;
+    },
+    githubAuth: async (_, { code }, { db }) => {
+      const { message, access_token, avatar_url, login, name } =
+        await authorizeWithGithub({
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        });
+
+      if (message) {
+        throw new Error(message);
+      }
+
+      const lastUserInfo = {
+        name,
+        githubLogin: login,
+        githubToken: access_token,
+        avatar: avatar_url,
+      };
+
+      await db.collection("users").replaceOne(
+        {
+          githubLogin: login,
+        },
+        lastUserInfo,
+        { upsert: true }
+      );
+
+      const users = await db
+        .collection("users")
+        .find({
+          githubLogin: login,
+        })
+        .toArray();
+
+      const [user] = users;
+
+      return {
+        user,
+        token: access_token,
+      };
     },
   },
   Photo: {
